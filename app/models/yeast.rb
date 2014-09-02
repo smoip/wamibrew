44b84662c61457ca83690e6becf2b1bebdfb5969
smoip <drsmoip@gmail.com>
2014-09-02 15:46:06 -0500

    yeast validations

--- app/models/yeast.rb
+++ app/models/yeast.rb
@@ -1,2 +1,4 @@
 class Yeast < ActiveRecord::Base
+  validates( :name, :uniqueness => { :case_sensitive => false } )
+  validates( :attenuation, :numericality => { :greater_than => 50, :less_than => 100 } )
 end
--- spec/models/yeast_spec.rb
+++ spec/models/yeast_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Yeast" do
-  before { @yeast = Yeast.new }
+  before { @yeast = FactoryGirl.create(:yeast) }
 
   subject { @yeast }
 
@@ -10,4 +10,19 @@ describe "Yeast" do
   it { should respond_to(:attenuation) }
 
   it { should be_valid }
+
+  describe "with invalid attributes" do
+    describe "with duplicate name" do
+      it "should be invalid" do
+        allow(@yeast).to receive(:name).and_return("WY1056")
+        expect(@yeast).not_to be_valid
+      end
+    end
+
+    describe "with out of range attenuation" do
+      before { @yeast.attenuation = 50 }
+      after { @yeast.attenuation = 75 }
+      it { should_not be_valid }
+    end
+  end
 end
