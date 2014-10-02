ca397d61e65035439ab0a4a13f724a4aec1afd0c
smoip <drsmoip@gmail.com>
2014-10-02 18:02:25 -0500

    add contact page, clean up static page tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 
   root 'static_pages#home'
   match '/new_recipe',  to: 'recipe#new', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#home', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # need contact page
 
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,16 +3,15 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home Page" do
-    before do
-      visit '/static_pages/home'
-    end
+    before { visit '/static_pages/home' }
+    subject { page }
+
+    it { should have_content('Wamibrew') }
     it "should have content 'Wamibrew'" do
       expect(page).to have_content('Wamibrew')
     end
-    it "should have the default title" do
-      expect(page).not_to have_title('| Home')
-      expect(page).to have_title('Wamibrew')
-    end
+    it { should_not have_title('Home') }
+    it { should have_title('Wamibrew') }
 
     it "should have footer links" do
       expect(page).to have_link('About', href: about_path)
@@ -31,28 +30,26 @@ describe "Static Pages" do
   end
 
   describe "Help Page" do
-    before do
-      visit '/static_pages/help'
-    end
-    it "should have content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-    # it { should have_title('| Help')}
-    it "should have title '| Help'" do
-      expect(page).to have_title('| Help')
-    end
+    before { visit '/static_pages/help' }
+    subject { page }
+
+    it { should have_content('Help') }
+    it { should have_title('| Help')}
   end
 
   describe "About Page" do
-    before do
-      visit '/static_pages/about'
-    end
-    it "should have content 'About'" do
-      expect(page).to have_content('About')
-    end
-    # it { should have_title('| About') }
-    it "should have title '| About'" do
-      expect(page).to have_title('| About')
-    end
+    before { visit '/static_pages/about' }
+    subject { page }
+
+    it { should have_content('About') }
+    it { should have_title('| About') }
+  end
+
+  describe "Contact Page" do
+    before { visit '/static_pages/contact' }
+    subject { page }
+
+    it { should have_content('Contact') }
+    it { should have_title('| Contact') }
   end
 end
