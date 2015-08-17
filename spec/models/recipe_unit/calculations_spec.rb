e07dcc4aa6bc0a5cbc4d800d7e10819cfefaf022
smoip <drsmoip@gmail.com>
2015-08-16 21:50:40 -0500

    finish calc unit tests

--- spec/models/recipe_unit/calculations_spec.rb
+++ spec/models/recipe_unit/calculations_spec.rb
@@ -11,6 +11,7 @@ describe "calculations" do
   let(:malt) { FactoryGirl.build(:malt) }
   let(:malt_1) { FactoryGirl.build(:malt, malt_yield: 0.7) }
   let(:yeast) { FactoryGirl.build(:yeast) }
+  let(:hop) { FactoryGirl.build(:hop) }
 
   describe "calc_abv" do
     context "og 1050 and fg 1010" do
@@ -86,10 +87,40 @@ describe "calculations" do
       expect(@recipe.pg_to_ep(1.050)).to be_within(0.01).of(50)
     end
   end
-  # calc_ibu
+
   describe "calc_ibu" do
+    before { allow(@recipe).to receive(:calc_indiv_ibu).and_return(48.362) }
+    context "with a single hop assigned" do
+      it "returns a single ibu value" do
+        allow(@recipe).to receive(:hops_to_array).and_return([[hop, [2.5, 60]]])
+        expect(@recipe.calc_ibu).to eq(48.4)
+      end
+    end
+    context "with multiple hops assigned" do
+      it "returns the combined ibu contributions of all assigned hops" do
+        allow(@recipe).to receive(:hops_to_array).and_return([[hop, [2.5, 60]], [hop, [1.0, 10]]])
+        expect(@recipe.calc_ibu).to eq(96.7)
+      end
+    end
   end
-  # calc_indiv_ibu(hop_ary)
+
+  describe "calc_indiv_ibu" do
+    before { allow(@recipe).to receive(:pull_hop_object).and_return(hop) }
+    it "calculates a single hop addition's ibu contribution" do
+      allow(@recipe).to receive(:pull_hop_amt).and_return(2.5)
+      allow(@recipe).to receive(:pull_hop_time).and_return(60)
+      allow(@recipe).to receive(:calc_hop_util).and_return(0.3081950635151269)
+      allow(@recipe).to receive(:calc_hop_ga).and_return(0.020000000000000018)
+      expect(@recipe.calc_indiv_ibu([ hop, [ 2.5, 60 ] ])).to eq(48.362422594828296)
+    end
+    it "calculates another hop addition's ibu contribution" do
+      allow(@recipe).to receive(:pull_hop_amt).and_return(1.5)
+      allow(@recipe).to receive(:pull_hop_time).and_return(10)
+      allow(@recipe).to receive(:calc_hop_util).and_return(0.06699216655991082)
+      allow(@recipe).to receive(:calc_hop_ga).and_return(0)
+      expect(@recipe.calc_indiv_ibu([ hop, [ 1.5, 10 ] ])).to eq(9.568000767092846)
+    end
+  end
 
   describe "rager_to_tinseth_q_and_d" do
     it "converts a rager ibu for > 30 min" do
@@ -123,9 +154,42 @@ describe "calculations" do
       end
     end
   end
-  #  calc_mcu(malt_ary)
-  # combine_mcu
-  # calc_srm
+
+  describe "calc_mcu" do
+    before { allow(@recipe).to receive(:pull_malt_object).and_return(malt) }
+    it "returns a malt color value for a single malt addition" do
+      allow(@recipe).to receive(:pull_malt_amt).and_return(5)
+      expect(@recipe.calc_mcu([malt, 5])).to eq(1.8)
+    end
+    it "returns a malt color value for a different malt addition" do
+      allow(@recipe).to receive(:pull_malt_amt).and_return(15)
+      expect(@recipe.calc_mcu([malt, 15])).to eq(5.4)
+    end
+  end
+
+  describe "combine_mcu" do
+    before { allow(@recipe).to receive(:calc_mcu).and_return(1.8) }
+    context "with single malt" do
+      it "returns a single mcu value" do
+        allow(@recipe).to receive(:malts_to_array).and_return([[malt, 5]])
+        expect(@recipe.combine_mcu).to eq(1.8)
+      end
+    end
+    context "with multiple malts assigned" do
+      it "returns a sum of mcu values" do
+        allow(@recipe).to receive(:malts_to_array).and_return([[malt, 5], [malt, 5]])
+        expect(@recipe.combine_mcu).to eq(3.6)
+      end
+    end
+  end
+
+  describe "calc_srm" do
+    it "converts mcu to srm" do
+      allow(@recipe).to receive(:combine_mcu).and_return(3.6)
+      @recipe.calc_srm
+      expect(@recipe.srm).to eq(3.6)
+    end
+  end
 
 
 end
